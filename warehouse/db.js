7bf4349c819baa6505cafbcae96db958d4264a21
zalesky <zalesky@ukr.net>
2016-04-09 00:26:45 +0300

    db

---
+++ warehouse/db.js
@@ -0,0 +1,43 @@
+var config = require('./config');
+var mongojs = require('mongojs')
+var db = mongojs(config.dbConnection, config.collections);
+
+function Warehouse(name) {
+    this.nameWarehouse = name;
+    this.date = Date.now();
+    this.available = true;
+};
+var warehouse = new Warehouse('superCompany');
+
+db.warehouse.insert(warehouse, function (err, saved) {
+    if (err || !saved) console.log("company not saved");
+    else console.log("company saved");
+});
+
+/*
+var schemaWarehouse = new mongoose.Schema({
+    nameWarehouse: String,
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    available: {
+        type: Boolean,
+        default: true
+    },
+    warehouseAccount: Number,
+    itemSet: [/!*{
+     id: Number,
+     amount: Number,
+     price: Number
+     }*!/]
+});
+
+var schemaItem = new mongoose.Schema({
+    name: String,
+    description: String,
+    image: String,
+    category: String
+});
+*/
+
