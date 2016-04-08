be0dd0f730946348c393c1085acde3807d9bf47c
asashay <asashayko@gmail.com>
2016-04-08 14:46:46 +0300

    added db design in text

---
+++ store/docs/db_design.js
@@ -0,0 +1,55 @@
+var store = {
+  _id: string,
+  name: string,
+  password: string,
+  account: number
+};
+
+var warehouse = {
+  _id: objectId,
+  name: string,
+  account: number,
+  storeId: id
+};
+
+var user = {
+  _id: objectId,
+  storeId: id,
+  name: string,
+  surname:  string,
+  email: string,
+  class: string,
+  password: string,
+  cart: [itemObject1, itemObject2, ... ],
+  account: number
+};
+
+var itemSet = {
+  storeId: id,
+  itemId: id,
+  originalPrice: number,
+  count: number
+};
+
+var item {
+  _id: objectId,
+  title: string,
+  description: string,
+  image: string,
+  price: number,
+  category: string
+};
+
+var transaction = {
+  _id: string,
+  accountFrom: number,
+  accountTo: number,
+  ammount: number
+};
+
+var order = {
+  _id: objectId,
+  date: date,
+  itemSet: [itemObject1, itemObject2, ... ],
+  transactionId: id
+}
--- store/index.js
+++ store/index.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
-})); 
+}));
 
 /////////
 require('./controllers').set(app);
@@ -17,7 +17,6 @@ var start = function(){
 	  console.log('Store app listening on port 3001');
 	});
 }
-
 
 module.exports = {
 	start: start
