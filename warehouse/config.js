7ed18ab643494cee29f06f19186b58b26c2b27d1
zalesky <zalesky@ukr.net>
2016-04-10 22:34:02 +0300

    work with api

--- warehouse/config.js
+++ warehouse/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: 3002,
 	dbConnection: 'mongodb://admin:qwertyuiop@ds017070.mlab.com:17070/warehouse',
-	collections: ['warehouses']
+	collections: ['warehouses','items']
 }
 
 
